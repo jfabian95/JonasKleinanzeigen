502a45d4494793f6c59e607533fc48105e899a45
jfabian95 <60751144+jfabian95@users.noreply.github.com>
2020-02-16 16:16:22 +0100

    Requirements
    added Requirements

--- my_app/urls.py
+++ my_app/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home
+from .views import home, new_search
 
 urlpatterns = [
     path('', home, name="Home"),
+    path('new_search', new_search, name="new_search")
 ]
